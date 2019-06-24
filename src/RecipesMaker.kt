1386f0165394ce07dd85412ae3de91291fb64eed
christiantc <christiantambo15@gmail.com>
2019-06-24 19:22:09 -0400

    Reto 2

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -14,9 +14,19 @@ fun main(args: Array<String>){
         val response: String? = readLine()
         println(response)
         when (response?.toInt()){
-            1 -> println("Escogiste la opcion 1\n")
-            2 -> println("Escogiste la opcion 2\n")
+            1 -> makeRecipe()
+            2 -> viewRecipe()
         }
     }while (response?.toInt()!=3)
 
+}
+
+fun makeRecipe() {
+    val menu = arrayListOf<String>("Agua","Leche","Carne","Verduras","Frutas","Cereal","Huevos","Aceites")
+    println("Hacer receta\nSelecciona por categoria el ingrediente que buscas")
+    menu.forEachIndexed() { index, s -> println("${index+1}. $s") }
+}
+
+fun viewRecipe(){
+    println("Ver mis recetas")
 }
